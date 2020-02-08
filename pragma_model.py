bffce6c5f6e994f5986ba64d82163a6d05af5709
REDxEYE <med45c@gmail.com>
2020-02-08 17:11:11 +0300

    moved ik to separate file

--- pragma_model.py
+++ pragma_model.py
@@ -17,34 +17,7 @@ class PragmaModelFlags(IntFlag):
     DontPrecacheTextureGroups = auto()
 
 
-class PragmaIKController(PragmaBase):
-    def __init__(self):
-        self.name = ""
-        self.type = ""
-        self.chain_len = 0
-        self.method = 0
-        self.key_values = {}
 
-    def from_file(self, reader: ByteIO):
-        self.name = reader.read_ascii_string()
-        self.type = reader.read_ascii_string()
-        self.chain_len = reader.read_uint32()
-        self.method = reader.read_uint32()
-        for _ in range(reader.read_uint32()):
-            key = reader.read_ascii_string()
-            self.key_values[key] = reader.read_ascii_string()
-
-    def to_file(self, writer: ByteIO):
-        writer.write_ascii_string(self.name)
-        writer.write_ascii_string(self.type)
-        writer.write_uint32(self.chain_len)
-        writer.write_uint32(self.method)
-        writer.write_uint32(len(self.key_values))
-        for k, v in self.key_values.items():
-            writer.write_ascii_string(k)
-            writer.write_ascii_string(v)
-
-    pass
 
 
 class PragmaModel(PragmaBase):
